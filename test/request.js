72c17c76e564c1253ddcac5264e10565705dc24e
Stufingo <kortgenie@gmail.com>
2019-06-12 17:19:37 +0200

    Setting up test framework

--- test/request.js
+++ test/request.js
@@ -1,4 +1,18 @@
 var Request = require('../source/request.js');
 var expect = require('chai').expect;
 
-describe('Request#')
+describe('Request#sign_string', function () {
+  it('should generate a valid hash', function () {
+    // 1. ARRANGE
+    var x = 5;
+    var y = 1;
+    var sum1 = x + y;
+
+    // 2. ACT
+    var sum2 = 6
+
+    // 3. ASSERT
+    expect(sum2).to.be.equal(sum1);
+  })
+
+})
